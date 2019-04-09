0247edecd3b26f92011e454ad8ed6e7924edbe1b
Hannah <hannah.justine.neumann@gmail.com>
2019-04-09 12:29:01 -0400

    Create toolkitValidation.groovy

---
+++ Utilities/Validation/toolkitValidation.groovy
@@ -0,0 +1,23 @@
+@groovy.transform.BaseScript com.ibm.dbb.groovy.ScriptLoader baseScript
+import com.ibm.dbb.repository.*
+import com.ibm.dbb.dependency.*
+import com.ibm.dbb.build.*
+
+//Get DBB toolkit product info
+version = new VersionInfo()
+println(" ")
+println("Info about this DBB installation")
+println("--------------------------------")
+println("DBB Version: " + version.getVersion())
+println("Build Number: " + version.getBuild())
+println("Release Date: " + version.getDate())
+println(" ")
+
+// execute IEFBR14
+def doAlmostNothing = new MVSExec().pgm("IEFBR14")
+def rc = doAlmostNothing.execute()
+
+println("Test MVSExec of IEFBR14")
+println("-----------------------")
+println("RC: " + rc)
+println(" ")
