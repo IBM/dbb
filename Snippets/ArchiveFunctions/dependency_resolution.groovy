e85cc2b2cc48d4bce9da26d8f6c60eb03981d561
kalebporter <kalebporter@ibm.com>
2019-05-20 10:32:15 -0400

    Fixes archive dependency resolution sample script

--- Snippets/ArchiveFunctions/dependency_resolution.groovy
+++ Snippets/ArchiveFunctions/dependency_resolution.groovy
@@ -1,16 +1,24 @@
-// example call: groovyz dependency_resolution.groovy /usr/PublicSamples/ Snippets/ArchiveFunctions/src/HELLOCBL.cbl
-
 import com.ibm.dbb.build.*
 import com.ibm.dbb.dependency.*
 
-def sourceDir = args[0] // e.g "/u/user/PublicSamples/Snippets/ArchiveFunctions/"
-def file = args[1] // e.g "src/HELLOCBL.cbl"
+def sourceDir = null
+def file = null
+
+try {
+	sourceDir = args[0] // e.g "/u/user/PublicSamples/"
+	file = args[1] // e.g "Snippets/ArchiveFunctions/src/HELLOCBL.cbl"
+}
+catch (Exception e){
+	println("Correct usage: groovyz dependency_resolution.groovy {sourceDir} {file}")
+	return
+}
+
 
 println("Source Directory: ${sourceDir}")
 println("File: ${file}")
 
 // resolve dependency
-def path = new DependencyPath().sourceDir(sourceDir).archive("archive/archive_copybook.tar").directory(sourceDir)
+def path = new DependencyPath().sourceDir(sourceDir).archive("Snippets/ArchiveFunctions/archive/archive_copybook.tar").directory(sourceDir)
 def rule = new ResolutionRule().library("SYSLIB").path(path)
 def resolver = new DependencyResolver().sourceDir(sourceDir).file(file).rule(rule)
 resolver.setScanner(new DependencyScanner())
