d7f066e30e0973d6d7dd5d1ddcc952cd52038774
Dennis Behm <dennis.behm@de.ibm.com>
2021-06-16 18:22:56 +0200

    Enhance UCD package traceability back to the Git provider (#79)
    * establish links in the UCD component version to git and  ci
    
    * switch to the new default -prop for the external artifact repos
    
    * streamline UCD component version properties
    
    * add gitBranch sample

--- Pipeline/CreateUCDComponentVersion/README.md
+++ Pipeline/CreateUCDComponentVersion/README.md
@@ -5,9 +5,27 @@ An important step in the pipeline is to generate a deployable package. This samp
 - generates the UCD shiplist ```shiplist.xml``` file
 - invokes the ```buztool.sh``` of UCD
 
-Example invocation:
+Example invocation (default):
 ```
-$DBB_HOME/bin/groovyz dbb-ucd-packaging.groovy --buztool /var/ucd/agent/bin/buztool.sh --workDir /var/ucd/packaging-samples --component MYCOMP --ar /var/ucd/agent/conf/artifactrepository/artifactory.properties --versionName MyVersion
+$DBB_HOME/bin/groovyz dbb-ucd-packaging.groovy --buztool /var/ucd/agent/bin/buztool.sh --workDir /var/build/job/dbb-outputdir --component MYCOMP --prop /var/ucd/agent/conf/artifactrepository/artifactory.properties --versionName MyVersion
+```
+
+Example to establish link to the pipeline url: 
+
+```
+$DBB_HOME/bin/groovyz dbb-ucd-packaging.groovy --buztool /var/ucd/agent/bin/buztool.sh --workDir /var/build/job/dbb-outputdir --component MYCOMP --prop /var/ucd/agent/conf/artifactrepository/artifactory.properties --versionName MyVersion --pipelineURL https://ci-server/job/MortgageApplication/34/
+```
+
+Example to establish link to the pipeline url and links for each deployable artifact to the git provider: 
+
+```
+$DBB_HOME/bin/groovyz dbb-ucd-packaging.groovy --buztool /var/ucd/agent/bin/buztool.sh --workDir /var/build/job/dbb-outputdir --component MYCOMP --prop /var/ucd/agent/conf/artifactrepository/artifactory.properties --versionName MyVersion --pipelineURL https://ci-server/job/MortgageApplication/34/ --repositoryInfoPropertiesFile /var/dbb/extensions/ucd-packaging/mortgageRepositoryProps.properties 
+```
+
+Example to establish link to the pipeline url and git branch: 
+
+```
+$DBB_HOME/bin/groovyz dbb-ucd-packaging.groovy --buztool /var/ucd/agent/bin/buztool.sh --workDir /var/build/job/dbb-outputdir --component MYCOMP --prop /var/ucd/agent/conf/artifactrepository/artifactory.properties --versionName MyVersion --pipelineURL https://ci-server/job/MortgageApplication/34/ --gitBranch development
 ```
 
 ## Processing flow
@@ -15,6 +33,8 @@ $DBB_HOME/bin/groovyz dbb-ucd-packaging.groovy --buztool /var/ucd/agent/bin/buzt
 - Read DBB's BuildReport.json from the pipeline work directory
 - Parse and extract build output information of records of type *ExecuteRecord* and *CopyToPDSRecord*
 - Optionally adds generic PropertyRecords, which are linked to the buildfile
+- Optionally adds links back to the ci pipeline build 
+- Optionally adds links to changes (git hashes) within the version control system
 - Generates shiplist.xml file
 - Invokes buztool.sh on USS with the generated options
 
@@ -24,21 +44,24 @@ $DBB_HOME/bin/groovyz <ussLocation>/dbb-ucd-packaging.groovy [options]
 
 required options:
 
- -b,--buztool <file>                Absolute path to UrbanCode Deploy
-                                    buztool.sh script
- -c,--component <name>              Name of the UCD component to
-                                    create version in
- -w,--workDir <dir>                 Absolute path to the DBB build
-                                    output directory
+ -b,--buztool <file>               		Absolute path to UrbanCode Deploy
+                                    		buztool.sh script
+ -c,--component <name>              		Name of the UCD component to
+                                    		create version in
+ -w,--workDir <dir>                 		Absolute path to the DBB build
+                                    		output directory
 
 optional options:
- -ar,--artifactRepository <arg>     Absolute path to Artifact Respository Server
-                                    Server connection file
- -prop,--propertyFile               Absolute path to property file. 
-                                    From UCD v7.1.x and greater it replace the -ar option.
- -v,--versionName <arg>             Name of the UCD component version
- -p,--preview                       Preview mode generate shiplist, but do
-                                    not run buztool.sh
+ -ar,--artifactRepository <arg>     		Absolute path to Artifact Respository Server
+                                    		Server connection file
+ -prop,--propertyFile <arg>         		Absolute path to property file. 
+                                    		From UCD v7.1.x and greater it replace the -ar option.
+ -v,--versionName <arg>             		Name of the UCD component version
+ -pURL,--pipelineURL <arg>			URL to the pipeline build result
+ -g,--gitBranch <arg>					Name of the git branch
+ -rpFile,--repositoryInfoPropertiesFile <arg>	Absolute path to the property file containing URL prefixes to git provider
+ -p,--preview                       		Preview mode generate shiplist, but do
+                                    		not run buztool.sh
 
 
 utility options
--- Pipeline/CreateUCDComponentVersion/dbb-ucd-packaging.groovy
+++ Pipeline/CreateUCDComponentVersion/dbb-ucd-packaging.groovy
@@ -19,6 +19,9 @@ import groovy.xml.MarkupBuilder
  *  -ar,--artifactRepository      Absolute path to Artifact Respository Server connection file (Optional)
  *  -prop,--propertyFile          Absolute path to property file (Optional). From UCD v7.1.x and greater it replace the -ar option.
  *  -p,--preview                  Preview, not executing buztool.sh
+ *  -pURL,--pipelineURL			  URL to the pipeline build result (Optional)
+ *  -g,--gitBranch				  Name of the git branch (Optional)
+ *  -rpFile,--repositoryInfoPropertiesFile  Absolute path to property file containing URL prefixes to git provider (Optional).
  *
  * notes:
  *   This script uses ship list specification and buztool parameters which are
@@ -50,6 +53,9 @@ import groovy.xml.MarkupBuilder
  *
  * Version 5 - 2021-04
  *  Take into account https://github.com/IBM/dbb/issues/76.
+ *  
+ * Version 6 - 2021-06
+ *  Take into account  https://github.com/IBM/dbb/issues/78
  */
 
 // start create version
@@ -87,7 +93,7 @@ println("** Find deployable outputs in the build report ")
 
 // Print warning, that extraction of COPY_TO_PDS records are not supported with older versions of the dbb toolkit. 
 dbbVersion = new VersionInfo().getVersion()
-println "   ! Buildrecord type TYPE_COPY_TO_PDS is supported with DBB toolkit 1.0.8 and higher. Identified DBB Toolkit version $dbbVersion. Extracting build records for TYPE_COPY_TO_PDS will be skipped."
+println "   * Buildrecord type TYPE_COPY_TO_PDS is supported with DBB toolkit 1.0.8 and higher. Extracting build records for TYPE_COPY_TO_PDS might not be available and skipped. Identified DBB Toolkit version $dbbVersion."
 
 // finds all the build outputs with a deployType
 def executes= buildReport.getRecords().findAll{
@@ -97,7 +103,7 @@ def executes= buildReport.getRecords().findAll{
 	} catch (Exception e){}	
 }
 
-// Remove unwanted outputs
+// Remove excluded outputs
 executes.each { 
 	def unwantedOutputs =  it.getOutputs().findAll{ o ->
 		o.deployType == null || o.deployType == 'ZUNIT-TESTCASE'
@@ -115,6 +121,19 @@ if ( count == 0 ) {
 
 executes.each { it.getOutputs().each { println("   ${it.dataset}, ${it.deployType}")}}
 
+// get DBB.BuildResultProperties records stored as generic DBB Record, see https://github.com/IBM/dbb-zappbuild/pull/95
+def buildResultRecord = buildReport.getRecords().find{
+		try {
+			it.getType()==DefaultRecordFactory.TYPE_PROPERTIES && it.getId()=="DBB.BuildResultProperties"
+		} catch (Exception e){}
+}
+
+def buildResultProperties = null
+
+if(buildResultRecord!=null){
+	buildResultProperties = buildResultRecord.getProperties()
+}
+
 // generate ship list file. specification of UCD ship list can be found at
 // https://www.ibm.com/support/knowledgecenter/SS4GSP_6.2.7/com.ibm.udeploy.doc/topics/zos_shiplistfiles.html
 println("** Generate UCD ship list file")
@@ -124,7 +143,16 @@ def xml = new MarkupBuilder(writer)
 xml.manifest(type:"MANIFEST_SHIPLIST"){
 	//top level property will be added as version properties
 	//requires UCD v6.2.6 and above
-	property(name : buildResult.getGroup() + "-buildResultUrl", value : buildResult.getUrl())
+	// Url to DBB Build result
+	property(name : "dbb-buildResultUrl", value : buildResult.getUrl())
+	// Url to CI pipeline
+	if (properties.pipelineURL) property(name : "ci-pipelineUrl", value : properties.pipelineURL )
+	// Git branch
+	if (properties.gitBranch) property(name : "ci-gitBranch", value : properties.gitBranch )
+	// Populate build result properties
+	if (buildResultProperties != null) buildResultProperties.each{
+		property(name:it.key, value:it.value)
+	}
 	//iterate through the outputs and add container and resource elements
 	executes.each{ execute ->
 		execute.getOutputs().each{ output ->
@@ -133,6 +161,7 @@ xml.manifest(type:"MANIFEST_SHIPLIST"){
 				resource(name:member, type:"PDSMember", deployType:output.deployType){
 					// add any custom properties needed
 					property(name:"buildcommand", value:execute.getCommand())
+					
 					// Only TYPE_EXECUTE Records carry options
 					if (execute.getType()==DefaultRecordFactory.TYPE_EXECUTE) property(name:"buildoptions", value:execute.getOptions())
 					// Sample to add additional properties. Here: adding db2 properties for a DBRM 
@@ -149,15 +178,31 @@ xml.manifest(type:"MANIFEST_SHIPLIST"){
 							}
 						}
 					}
-
-					// add source information
-					inputs(url : ""){
-						input(name : execute.getFile(), compileType : "Main")
+					def githash = "" // set empty
+					if (buildResultProperties != null){
+						// get git references from build properties
+						def gitproperty = buildResultProperties.find{
+							it.key.contains(":githash:") && execute.getFile().contains(it.key.substring(9))
+						}
+						if (gitproperty != null ) 
+						{
+							githash = gitproperty.getValue()
+							// set properties in shiplist
+							property(name:"githash", value:githash)
+							if(properties.git_commitURL_prefix) property(name:"git-link-to-commit", value:"${properties.git_commitURL_prefix}/${githash}")
+						}
+					}
+					// add source information in the input column
+					inputs(url : "${githash}"){
+						inputUrl = (buildResultProperties != null && properties.git_treeURL_prefix && githash!="") ? "${properties.git_treeURL_prefix}/${githash}/"+ execute.getFile() : ""
+						input(name : execute.getFile(), compileType : "Main", url : inputUrl)
+						// dependencies (not used today)
 						dependencies.each{
 							if(it.getId() == execute.getFile()){
 								it.getAllDependencies().each{
+									
 									def displayName = it.getFile()? it.getFile() : it.getLname()
-									input(name : displayName, compileType : it.getCategory())
+									input(name : displayName , compileType : it.getCategory())
 								}
 							}
 						}
@@ -260,6 +305,10 @@ def parseInput(String[] cliArgs){
 	cli.prop(longOpt:'propertyFile', args:1, argName:'propertyFileSettings', 'Absolute path to property file (Optional). From UCD v7.1.x and greater it replace the -ar option')
 	cli.v(longOpt:'versionName', args:1, argName:'versionName', 'Name of the UCD component version')
 	cli.p(longOpt:'preview', 'Preview mode - generate shiplist, but do not run buztool.sh')
+	cli.pURL(longOpt:'pipelineURL', args:1,'URL to the pipeline build result (Optional)')
+	cli.g(longOpt:'gitBranch', args:1,'Name of the git branch (Optional)')
+	cli.rpFile(longOpt:'repositoryInfoPropertiesFile', args:1,'Absolute path to property file containing URL prefixes to git provider (Optional)')
+		
 	cli.h(longOpt:'help', 'Prints this message')
 	def opts = cli.parse(cliArgs)
 	if (opts.h) { // if help option used, print usage and exit
@@ -278,6 +327,15 @@ def parseInput(String[] cliArgs){
 		if (buildProperties.workDir != null)
 			properties.workDir = buildProperties.workDir
 	}
+	
+	// load properties from repositoryInfoPropertiesFile
+	if (opts.rpFile){
+		def repositoryPropFile = new File("$opts.rpFile")
+		if (repositoryPropFile.exists()){
+			repositoryPropFile.withInputStream { 
+				properties.load(it) }
+		}
+	}
 
 	// set command line arguments
 	if (opts.w) properties.workDir = opts.w
@@ -286,6 +344,8 @@ def parseInput(String[] cliArgs){
 	if (opts.ar) properties.artifactRepositorySettings = opts.ar
 	if (opts.prop) properties.propertyFileSettings = opts.prop
 	if (opts.v) properties.versionName = opts.v
+	if (opts.pURL) properties.pipelineURL = opts.pURL
+	if (opts.g) properties.gitBranch = opts.g
 	properties.preview = (opts.p) ? 'true' : 'false'
 
 	// validate required properties
