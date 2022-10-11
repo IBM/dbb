a89001b523959f503f0454f2573bdcc07211f552
Jack Murray <Jack.Murray@ibm.com>
2022-10-11 09:43:45 -0400

    Update BuildReportPruner
    Replace RepositoryClient with MetadataStore

--- Utilities/BuildReportPruner/Saver.groovy
+++ Utilities/BuildReportPruner/Saver.groovy
@@ -1,10 +1,10 @@
 import com.ibm.dbb.build.*
 import com.ibm.dbb.build.report.*
-import com.ibm.dbb.repository.*
+import com.ibm.dbb.metadata.*
 import com.ibm.dbb.dependency.*
 
-//checking that the user has inputed some parameter
-if(args.length == 0){
+// checking that the user has inputed some parameter
+if (args.length == 0) {
 	println("Please add an argument to the script. Run again with -h or --help to view options.")
 	System.exit(0)
 }
@@ -14,223 +14,224 @@ def scriptDir = new File(getClass().protectionDomain.codeSource.location.path).p
 File sourceFile = new File("$scriptDir/argParser.groovy")
 Class groovyClass = new GroovyClassLoader(getClass().getClassLoader()).parseClass(sourceFile)
 GroovyObject tools = (GroovyObject) groovyClass.newInstance()
-client = tools.setUpClient("Save")
+store = tools.setUpStore()
 def opts = tools.parseArgs(args)
 preview = false
-if(opts.p){
+if (opts.p) {
 	preview = true
 }
 
-//will save on default, user can enter -u to toggle unsave
+// will save on default, user can enter -u to toggle unsave
 state = "yes"
-if(opts.u){
+if (opts.u) {
 	state="no"
 }
 
-//methods
+// methods
 
-def checkGroupName(String name){
-	//Will check if the entered group name exists
-	def groups = client.listBuildResultGroups()
-	if(!groups.contains(name)){
+def checkGroupName(String name) {
+	// Will check if the entered group name exists
+	def groups = store.listBuildResultGroups()
+	if (!groups.contains(name)) {
 		println("Sorry, that group does not exist. Check the spelling and try again.")
 		System.exit(0)
 	}
 }
 
-def saveBR(buildRes,state){
-	//Will toggle the save flag for the specified build
-	if(buildRes == null){
+def saveBR(buildRes, state) {
+	// Will toggle the save flag for the specified build
+	if (buildRes == null) {
 		println("Sorry, that build does not exist. Check spelling and try again.")
 		System.exit(0)
 	}
-	if(preview){
+	if (preview) {
 		println("This is a preview:")
-	}else{
-		buildRes.setProperty("save",state)
+	}
+	else {
+		buildRes.setProperty("save", state)
 		buildRes.save()
 	}
-	if(state.equals("yes")){
+	if (state.equals("yes")) {
 		println("Build: " + buildRes.getLabel() + " will now be saved")
-	}else{
+	}
+	else {
 		println("Build: " + buildRes.getLabel() + " will now not be saved")
 	}
 	println()
 }
 
-def saveBuilds(currGroup, numToKeep = currGroup.size()){
-	//Will toggle the save flag for the builds (or the number of specified builds) in the specified group.
+def saveBuilds(currGroup, numToKeep = currGroup.size()) {
+	// Will toggle the save flag for the builds (or the number of specified builds) in the specified group.
 	def message = (state.equals("yes")) ? "Not being saved: " : "Not being unsaved: "
 	def i = 0
 	currGroup.each{ buildResult->
-		if(i<numToKeep){
-			saveBR(buildResult,state)
+		if (i < numToKeep) {
+			saveBR(buildResult, state)
 			i++
-		}else{
+		}
+		else {
 			println(message + buildResult.getLabel())
 		}
 	}
 }
 
-//end methods
+// end methods
 
 
 
-if(opts.a){
-	//Will toggle the save flag for all builds
-	def builds = client.getAllBuildResults(new HashMap<String,String>())
-	builds.each{ buildResult ->
-		saveBR(buildResult,state)
+if (opts.a) {
+	// Will toggle the save flag for all builds
+	def groups = store.listBuildResultGroups()
+	groups.each{ group ->
+		def builds = store.getBuildResults(group)
+		builds.each{ buildResult ->
+			saveBR(buildResult, state)
+		}
 	}
-	
-}else if(opts.d){
-	//Will toggle the save flag for the build with the specified ID
-	def buildResult = client.getBuildResult((long)opts.d.toInteger())
-	saveBR(buildResult,state)
-	
-}else if(opts.o){
-	//Will toggle the save flag for a specified amount of the most recent builds 
+}
+else if (opts.o) {
+	// Will toggle the save flag for a specified amount of the most recent builds 
 	def queryParms = ["orderBy":"lastUpdated","order":"DESC"]
-	if(opts.g){
-		//checking if a group has been specified
+	if (opts.g) {
+		// checking if a group has been specified
 		def groupName = opts.g
 		checkGroupName(groupName)
-		queryParms.put("group",groupName)
+		queryParms.put("group", groupName)
 	}
 	
 	def message = (state.equals("yes")) ? "Now saving " : "Now unsaving "
 	
-	if(opts.c||opts.e||opts.w){
-		//checking if any states has been specified
-		if(opts.c){
+	if (opts.c || opts.e || opts.w) {
+		// checking if any states has been specified
+		if (opts.c) {
 			println(message+ "clean builds:")
 			queryParms.put("status","0")
-			def currGroup = client.getAllBuildResults(queryParms)
-			saveBuilds(currGroup,opts.c.toInteger())
+			def currGroup = store.getBuildResults(queryParms)
+			saveBuilds(currGroup, opts.c.toInteger())
 		}
-		if(opts.e){
+		if (opts.e) {
 			println(message + "error builds:")
 			queryParms.put("status","1")
-			def currGroup = client.getAllBuildResults(queryParms)
-			saveBuilds(currGroup,opts.e.toInteger())
+			def currGroup = store.getBuildResults(queryParms)
+			saveBuilds(currGroup, opts.e.toInteger())
 		}
-		if(opts.w){
+		if (opts.w) {
 			println(message + "warning builds:")
 			queryParms.put("status","2")
-			def currGroup = client.getAllBuildResults(queryParms)
-			saveBuilds(currGroup,opts.w.toInteger())
+			def currGroup = store.getBuildResults(queryParms)
+			saveBuilds(currGroup, opts.w.toInteger())
 		}
-		
-	}else{
-		def currGroup = client.getAllBuildResults(queryParms)
-		saveBuilds(currGroup,opts.o.toInteger())
 	}
-	
-}else if(opts.g){
+	else {
+		def currGroup = store.getBuildResults(queryParms)
+		saveBuilds(currGroup, opts.o.toInteger())
+	}
+}
+else if (opts.g) {
 	def groupName = opts.g
 	checkGroupName(groupName)
 	
-	if(opts.l){
-		//checking if a label has been specified
-		def buildResult = client.getBuildResult(groupName, opts.l)
-		saveBR(buildResult,state)
+	if (opts.l) {
+		// checking if a label has been specified
+		def buildResult = store.getBuildResult(groupName, opts.l)
+		saveBR(buildResult, state)
 		System.exit(0)
 	}
 	
 	def queryParms = ["group":groupName,"orderBy":"lastUpdated","order":"DESC"]
-	if(opts.c||opts.e||opts.w){
+	if (opts.c || opts.e || opts.w) {
 		def message = (state.equals("yes")) ? "Now saving " : "Now unsaving "
-		//checking if a state has been specified
-		if(opts.c){
+		// checking if a state has been specified
+		if (opts.c) {
 			println(message + "clean builds: ")
-			queryParms.put("status","0")
-			def currGroup = client.getAllBuildResults(queryParms)
+			queryParms.put("status", "0")
+			def currGroup = store.getBuildResults(queryParms)
 			saveBuilds(currGroup)
 		}
-		if(opts.e){
+		if (opts.e) {
 			println(message + "error builds:")
 			queryParms.put("status","1")
-			def currGroup = client.getAllBuildResults(queryParms)
+			def currGroup = store.getBuildResults(queryParms)
 			saveBuilds(currGroup)
 		}
-		if(opts.w){
+		if (opts.w) {
 			println(message + "warning builds:")
-			queryParms.put("status","2")
-			def currGroup = client.getAllBuildResults(queryParms)
+			queryParms.put("status", "2")
+			def currGroup = store.getBuildResults(queryParms)
 			saveBuilds(currGroup)
 		}
-	}else{
-		def currGroup = client.getAllBuildResults(queryParms)
+	}
+	else {
+		def currGroup = store.getBuildResults(queryParms)
 		saveBuilds(currGroup)
 	}
-	
-	
-}else if(opts.i){
+}
+else if (opts.i) {
 	def check = System.console().readLine("Save or unsave? (save/unsave): ")
 	option = System.console().readLine("Enter which option you'd like to initiate: ")
-	if(check.equals("unsave")) state = "no"
+	if (check.equals("unsave")) state = "no"
 	def message = (state.equals("yes")) ? "Now saving " : "Now unsaving "
 	
 	
-	if(option.equals("-a")||option.equals("--all")){
+	if (option.equals("-a") || option.equals("--all")) {
 		def selection = System.console().readLine "Are you sure you want to $check all builds in all groups? (yes/no): "
 		
-		
-		if(selection.equals("yes")||selection.equals("YES")||selection.equals("Yes")||selection.equals("y")){
-			def builds = client.getAllBuildResults(new HashMap<String,String>())
-			builds.each{ buildResult ->
-				saveBR(buildResult,state)
+		if (selection.equals("yes") || selection.equals("YES") || selection.equals("Yes") || selection.equals("y")) {
+			
+			def groups = store.listBuildResultGroups()
+			groups.each{ group ->
+				def builds = store.getBuildResults(group)
+				builds.each{ buildResult ->
+					saveBR(buildResult, state)
+				}
 			}
-		}else{
+		}
+		else {
 			println("Not changing build results.")
 		}
-		
-		
-	}else if(option.equals("-d")||option.equals("--id")){
-		def id = System.console().readLine "Specify the ID of the build you'd like to $check: "
-		def buildResult = client.getBuildResult((long)id)
-		saveBR(buildResult,state)
-		
-	}else if(option.equals("-g")||option.equals("--group")){
+	}
+	else if (option.equals("-g") || option.equals("--group")) {
 		
 		def groupName = System.console().readLine "Specify Group Name: "
 		checkGroupName(groupName)
 		
 		def selection = System.console().readLine "Would you like to specify a label name?(yes/no) "
-		if(selection.equals("yes")||selection.equals("YES")||selection.equals("Yes")||selection.equals("y")){
+		if (selection.equals("yes") || selection.equals("YES") || selection.equals("Yes") || selection.equals("y")) {
 			def labelName = System.console().readLine "Specify a label name: "
-			def buildResult = client.getBuildResult(groupName, labelName)
-			saveBR(buildResult,state)
+			def buildResult = store.getBuildResult(groupName, labelName)
+			saveBR(buildResult, state)
 			System.exit(0)
 		}
 		
 		def queryParms = ["group":groupName]
 		def selection2 = System.console().readLine "Would you like to specify a specific state to $check? (yes/no)"
-		if(selection2.equals("yes")||selection2.equals("YES")||selection2.equals("Yes")||selection2.equals("y")){
+		if (selection2.equals("yes") || selection2.equals("YES") || selection2.equals("Yes") || selection2.equals("y")) {
 			def statusIn = System.console().readLine "Please specify a state: (clean/error/warning)"
 			def status = ""
-			if(statusIn.equals("clean")||statusIn.equals("CLEAN")) status = "0"
-			if(statusIn.equals("error")||statusIn.equals("ERROR")) status = "1"
-			if(statusIn.equals("warning")||statusIn.equals("WARNING")) status = "2"
-			if(status.equals("")){
+			if (statusIn.equals("clean") || statusIn.equals("CLEAN")) status = "0"
+			if (statusIn.equals("error") || statusIn.equals("ERROR")) status = "1"
+			if (statusIn.equals("warning") || statusIn.equals("WARNING")) status = "2"
+			if (status.equals("")) {
 				println("Please enter a valid status: either clean, error, or warning")
 				System.exit(0)
-			}else{
+			}
+			else {
 				queryParms.put("status",status)
 			}
 		}
 		
-		def currGroup = client.getAllBuildResults(queryParms)
+		def currGroup = store.getBuildResults(queryParms)
 		currGroup.each{ buildResult ->
 			saveBR(buildResult,state)
 		}
-	}else if(option.equals("-o")||option.equals("--oldest")){
+	}
+	else if (option.equals("-o") || option.equals("--oldest")) {
 		
 		def strCleanToKeep = System.console().readLine "Please specify the amount of clean builds to $check.\nEnter -1 to ignore clean build results: "
 		def strErrorToKeep = System.console().readLine "Please specify the amount of error builds to $check.\nEnter -1 to ignore error build results: "
 		def strWarningToKeep = System.console().readLine "Please specify the amount of warning builds to $check.\nEnter -1 to ignore warning build results: "
 	
-		if(!strCleanToKeep.isNumber()||!strErrorToKeep.isNumber()||!strWarningToKeep.isNumber()){
+		if (!strCleanToKeep.isNumber() || !strErrorToKeep.isNumber() || !strWarningToKeep.isNumber()) {
 			println("Please only provide numbers for the amounts of builds you'd like to $check")
 			System.exit(0)
 		}
@@ -242,38 +243,39 @@ if(opts.a){
 		def queryParms = ["orderBy":"lastUpdated","order":"DESC"]
 		
 		def selection = System.console().readLine "Would you like to specify a specific group to $check? (yes/no): "
-		if(selection.equals("yes")||selection.equals("YES")||selection.equals("Yes")||selection.equals("y")){
-			
+		if (selection.equals("yes") || selection.equals("YES") || selection.equals("Yes") || selection.equals("y")) {
 			def groupName = System.console().readLine "Specify Group Name: "
 			checkGroupName(groupName)
-			queryParms.put("group",groupName)
+			queryParms.put("group", groupName)
 		}
 			
-		if(numCleanToKeep != -1){
+		if (numCleanToKeep != -1) {
 			println(message + "clean builds:")
-			queryParms.put("status","0")
-			def currGroup = client.getAllBuildResults(queryParms)
-			saveBuilds(currGroup,numCleanToKeep)
+			queryParms.put("status", "0")
+			def currGroup = store.getBuildResults(queryParms)
+			saveBuilds(currGroup, numCleanToKeep)
 		}
-		if(numErrorToKeep != -1){
+		if (numErrorToKeep != -1) {
 			println(message + "error builds:")
-			queryParms.put("status","1")
-			def currGroup = client.getAllBuildResults(queryParms)
+			queryParms.put("status", "1")
+			def currGroup = store.getBuildResults(queryParms)
 			saveBuilds(currGroup,numErrorToKeep)
 		}
-		if(numWarningToKeep != -1){
+		if (numWarningToKeep != -1) {
 			println(message + "warning builds:")
-			queryParms.put("status","2")
-			def currGroup = client.getAllBuildResults(queryParms)
-			saveBuilds(currGroup,numWarningToKeep)
+			queryParms.put("status", "2")
+			def currGroup = store.getBuildResults(queryParms)
+			saveBuilds(currGroup, numWarningToKeep)
 		}
 	
-	}else{
+	}
+	else {
 		println("Sorry, please enter a recognized parameter. Run again with -h or --help to see all the options")
 		System.exit(0)
 	}
 	
-}else{
+}
+else {
 	println("Sorry, please enter a recognized parameter for Saver.groovy. Run again with -h or --help to see all the options")
 	System.exit(0)
 }
--- Utilities/BuildReportPruner/argParser.groovy
+++ Utilities/BuildReportPruner/argParser.groovy
@@ -17,110 +17,90 @@ def parseArgs(String[] cliArgs){
 	cli.c(longOpt: 'cleanBuildsToKeep',args:1,optionalArg:true,argName:'cleanBuilds', 'Number of clean builds to keep from pruning')
 	cli.e(longOpt: 'errorBuildsToKeep',args:1,optionalArg:true, argName:'errorBuilds','Number of error builds to keep from pruning')
 	cli.w(longOpt: 'warningBuildsToKeep',args:1,optionalArg:true, argName:'warningBuilds','Number of warning builds to keep from pruning')
-	cli.d(longOpt: 'id',args:1,optionalArg:true,argName:'id','Specify an ID')
 	cli.i(longOpt: 'interactive','Toggle the interactive mode')
 	cli.p(longOpt:'preview','Preview tag')
 	
 	def opts = cli.parse(cliArgs)
-	if(opts.h){
+	if (opts.h) {
 		printHelp()
 		System.exit(0)
 	}
 	
-	if(opts.g && opts.g instanceof Boolean){
+	if (opts.g && opts.g instanceof Boolean) {
 		println("-g needs you to specify a group name. Run with -h to view details.")
 		System.exit(0)
 	}
 	
-	if(opts.d && opts.d instanceof Boolean){
-		println("-d needs you to specify an ID. Run with -h to view details.")
-		System.exit(0)
-	}
-	
-	if(opts.l && opts.l instanceof Boolean){
+	if (opts.l && opts.l instanceof Boolean) {
 		println("-l needs you to specify a label name. Run with -h to view details.")
 		System.exit(0)
 	}
 	
-	if(opts.o){
-		if((opts.c && opts.c instanceof Boolean) || (opts.e && opts.e instanceof Boolean) || (opts.w && opts.w instanceof Boolean)){
+	if (opts.o) {
+		if ((opts.c && opts.c instanceof Boolean) || (opts.e && opts.e instanceof Boolean) || (opts.w && opts.w instanceof Boolean)) {
 			println("-c,-e,and -w need you to specify the number of builds to keep. Run with -h to view details.")
 			System.exit(0)
 		}
-		if(!opts.c&&!opts.e&&!opts.w&&opts.o instanceof Boolean){
+		if (!opts.c&&!opts.e&&!opts.w&&opts.o instanceof Boolean) {
 			println("Sorry, please specify the number of builds to keep for -o. Run with -h to view details.")
 			System.exit(0)
 		}
 	}
 	
-	if(opts.d&&(!opts.d.isNumber())){
-		println("Sorry, please enter a number as the parameter for -d")
-		System.exit(0)
-	}
-	
-	if(opts.u){
+	if (opts.u) {
 		user = "Unsave"
 	}
 	return opts
 }
 
 
-def setUpClient(caller){
-	user = caller
-	def client = new RepositoryClient()
-	
+def setUpStore() {
 	def properties = BuildProperties.getInstance()
 	def scriptDir = new File(getClass().protectionDomain.codeSource.location.path).parent
 	def buildPropFile = new File("$scriptDir/user.properties")
-	if (buildPropFile.exists()){
+	if (buildPropFile.exists()) {
 		BuildProperties.load(buildPropFile)
 	}
-	
-	
-	if(!properties.url||!properties.username){
-		println("You must update the user.properties file first to be able to use the Pruner")
-		System.exit(0)
+
+	if (properties.db2 == "true") {
+		if (!properties.url || !properties.userId) {
+			println("You must update the user.properties file first to be able to use the Pruner")
+			System.exit(0)
+		}
+
+		try {
+			if (properties.password != null) {
+				return MetadataStoreFactory.createDb2MetadataStore(properties.url, properties.userId, properties.password)
+			}
+			else if (properties.passwordFile != null) {
+				return MetadataStoreFactory.createDb2MetadataStore(properties.url, properties.userId, new File(properties.passwordFile))
+			}
+			else {
+				println("Please enter either a password or the path for a password file in user.properties")
+				System.exit(0)
+			}
+		}
+		catch (BuildException e) {
+			println("Faled to instantiate Db2 Metadata Store: " + e.getMessage())
+		}
 	}
-	 
-	client.setUrl(properties.url)
-	client.setUserId(properties.username)
-	if(properties.password!=null){
-		client.setPassword(properties.password)
-	}else if(properties.passwordFile!=null){
-		client.setPasswordFile(new File(properties.passwordFile))
-	}else{
-		println("Please enter either a password or the path for a password file in user.properties")
-		System.exit(0)
+	else {
+		return MetadataStoreFactory.createFileMetadataStore(properties.dir)
 	}
-	client.setForceSSLTrusted(true)
-	client.getAllBuildResults(new HashMap())
-	def status = client.getLastStatusCode()
-	if(status == 401){
-		println("Please check that the password and username are correct in the properties file.")
-		System.exit(0)
-	}
-	if(status == 404){
-		println("Please check that the url is correct in the properties file.")
-		System.exit(0)
-	}
-	return client
 }
 
-def printHelp(){
+def printHelp() {
 	def message = (user.equals("Prune")) ? ", the rest will be pruned" : ""
 	
 	println()
 	println("*****************"+user+"r.groovy*****************")
 	println("Options so far:")
-	if(!user.equals("Prune")){
+	if (!user.equals("Prune")) {
 		println("-u,--unsave")
 		println(": will turn off the save flag for the specified build(s)")
 	}
 	println("-a,--all")
 	println(": will " + user.toLowerCase() + " all builds in all groups")
-	println()
-	println("-d,--id <ID>")
-	println(":will " + user.toLowerCase() + " the build with the specified ID")
 	println()
 	println("-g,--group <group name>")
 	println(": will " + user.toLowerCase() + " all the builds in the specified group")
