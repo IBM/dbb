9c055635e4fe452b3bd2723fac954b1031196975
Kai McGregor <kmcgreg@ibm.com>
2023-03-09 11:08:16 -0500

    Added new testcases from 2.x refactor
    Signed-off-by: Kai McGregor <kmcgreg@ibm.com>

--- Migration/ReportMigration/tests/StaticReportMigrationTests.groovy
+++ Migration/ReportMigration/tests/StaticReportMigrationTests.groovy
@@ -5,6 +5,8 @@ import com.ibm.dbb.build.internal.Utils;
 
 import java.util.concurrent.TimeUnit;
 import java.lang.Thread;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.AfterAll;
@@ -12,24 +14,31 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.ClassOrderer;
+import org.junit.jupiter.api.TestClassOrder;
 
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 
-
+@TestClassOrder(ClassOrderer.OrderAnnotation.class)
 class StaticReportMigrationTests {
     private static final String GROUP = "Static-Report-Migration-Test";
+    private static final String GROUP2 = "Static-Report-Migration-Test-2";
     private static final String LABEL = "buildresult";
     private static final String URL_KEY = "test-url";
     private static final String ID_KEY = "test-id";
     private static final String PW_FILE_KEY = "test-pwFile";
 
     private static File testDir = new File(StaticReportMigrationTests.getProtectionDomain().getCodeSource().getLocation().getPath()).getParentFile();
-    private static String script = new File(testDir, "../bin/static-report-migration.sh").getPath();
+    private static String listScript = new File(testDir, "../bin/create-migration-list.sh").getPath();
+    private static String migrateScript = new File(testDir, "../bin/migrate-list.sh").getPath();
+    private static File jsonFile = new File("list.json");
 
     private static String url;
     private static String id;
@@ -38,92 +47,6 @@ class StaticReportMigrationTests {
 
     private static final int maxNormError = 202;
     private static final int maxDeleteError = 405;
-
-    @Nested
-    @TestInstance(Lifecycle.PER_CLASS)
-    class IntegrationTests {
-        @BeforeAll
-        static void setupCollection() throws Exception {
-            System.out.println("Setting up collection.");
-            client.setErrorStatusCode(maxDeleteError);
-            client.deleteBuildResults(GROUP);
-            client.deleteCollection(GROUP);
-            client.setErrorStatusCode(maxNormError);
-            
-            client.createCollection(GROUP);
-            BuildResult newResult = client.createBuildResult(GROUP, LABEL);
-            newResult.setState(BuildResult.COMPLETE);
-
-            String samplesFolder = "samples/";
-            // Report data is labled with the version used to create it, in case of differences between versions
-            newResult.setBuildReportData(new FileInputStream(new File(testDir, samplesFolder + "result-data-1.1.3.json")));
-            newResult.setBuildReport(new FileInputStream(new File(testDir, samplesFolder + "report.html")));
-            newResult.save();
-
-            System.out.println("Asserting test file content.");
-            String metadataString = '{"date":"28-Feb-2022 17:26:26","build":"151","id":"DBB API Version","type":"VERSION","version":"1.1.3"}';
-            List<BuildResult> results = client.getAllBuildResults(Collections.singletonMap(RepositoryClient.GROUP, GROUP));
-            for (BuildResult result : results) {
-                assertEquals(GROUP, result.getGroup());
-                assertEquals(LABEL, result.getLabel());
-                assertTrue(Utils.readFromStream(result.fetchBuildReport(), "UTF-8").contains(metadataString));
-                assertTrue(Utils.readFromStream(result.fetchBuildReportData(), "UTF-8").contains(metadataString));
-            }
-            assertTrue(results.size() == 1);
-        }
-
-        @Test
-        void migrationTest() {
-            System.out.println("Running migration test.");
-            List<String> command = new ArrayList<>();
-            command.add(script);
-            command.add("--url");
-            command.add(url);
-            command.add("--id");
-            command.add(id);
-            command.add("--pwFile");
-            command.add(passwordFile.getPath());
-            command.add("--grp");
-            command.add(GROUP);
-            Map<String, String> output = runMigrationScript(command, 0);
-            assertTrue(output.get("err").trim().isEmpty(), String.format("Error stream is not empty\nOUT:\n%s\n\nERR:\n%s", output.get("out"), output.get("err")));
-            validateResults();
-        }
-
-        @Test
-        void badPasswordFileTest() {
-            System.out.println("Running bad password file test.");
-            List<String> command = new ArrayList<>();
-            command.add(script);
-            command.add("--url");
-            command.add(url);
-            command.add("--id");
-            command.add(id);
-            command.add("--pwFile");
-            command.add("~/nonexistantfile");
-            command.add("--grp");
-            command.add(GROUP);
-            Map<String, String> output = runMigrationScript(command, 2);
-            assertTrue(output.get("out").contains("There was an issue reading your password file"));
-        }
-
-        @Test
-        void badPasswordTest() {
-            System.out.println("Running bad password test.");
-            List<String> command = new ArrayList<>();
-            command.add(script);
-            command.add("--url");
-            command.add(url);
-            command.add("--id");
-            command.add(id);
-            command.add("--pw");
-            command.add("BADPASSWORD");
-            command.add("--grp");
-            command.add(GROUP);
-            Map<String, String> output = runMigrationScript(command, 2);
-            assertTrue(output.get("out").contains("There was an issue connecting to the Repository Client"));
-        }
-    }
 
     @BeforeAll
     static void setupStore() throws IOException {
@@ -148,6 +71,8 @@ class StaticReportMigrationTests {
         client.setUserId(id);
         client.setPasswordFile(passwordFile);
         client.setErrorStatusCode(maxNormError);
+
+        setupCollection();
     }
 
     @AfterAll
@@ -156,6 +81,259 @@ class StaticReportMigrationTests {
         client.setErrorStatusCode(maxDeleteError);
         client.deleteBuildResults(GROUP);
         client.deleteCollection(GROUP);
+    }
+
+    @Test
+    void testVersion() {
+        System.out.println("Running version test.");
+        File testVersion = new File(testDir, "samples/version.properties");
+
+        File versionPackage = new File("com/ibm/dbb/build/internal/version.properties");
+        versionPackage.getParentFile().mkdirs();
+        Files.copy(testVersion.toPath(), versionPackage.toPath());
+        String version = VersionInfo.getInstance().getVersion();
+        VersionInfo.staticReset();
+        try {
+            // Update version file within jar
+            List<String> command = new ArrayList<>();
+            command.add("jar");
+            command.add("-uf");
+            command.add(EnvVars.getHome() + "/lib/dbb.core_" + version + ".jar");
+            command.add(versionPackage.getPath());
+            runProcess(command, 0);
+
+            // Run tests on the create-list script
+            String errorMessage = "DBB Version 1.1.3 is not compatable with this tool";
+            command = new ArrayList<>();
+            command.add(listScript);
+            Map<String, String> output = runProcess(command, 1);
+            assertTrue(output.get("out").contains(errorMessage));
+
+            // Run test on the migration script
+            command = new ArrayList<>();
+            command.add(migrateScript);
+            output = runProcess(command, 1);
+            assertTrue(output.get("out").contains(errorMessage));
+        } finally {
+            // Replace the version file with the original
+            versionPackage.delete();
+            Files.copy(Paths.get(EnvVars.getHome() + "/bin/version.properties"), versionPackage.toPath());
+            List<String> command = new ArrayList<>();
+            command.add("jar");
+            command.add("-uf");
+            command.add(EnvVars.getHome() + "/lib/dbb.core_" + version + ".jar");
+            command.add(versionPackage.getPath());
+            try {
+                runProcess(command, 0);
+            } catch (Exception error) {
+                throw new Exception("Failed to reset DBB version info back to original state.", error);
+            } finally {
+                // Clear temp folder structure
+                while (versionPackage != null) {
+                    versionPackage.delete();
+                    versionPackage = versionPackage.getParentFile();
+                }
+            }
+        }
+        // Check version was reset properly
+        assertEquals(VersionInfo.getInstance().getVersion(), version);
+    }
+
+    @Nested
+    @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+    @Order(1)
+    class ListCreationTests {
+        @Test
+        @Order(1)
+        void testWildcard() {
+            System.out.println("Running group wildcard tests.");
+            List<String> command = new ArrayList<>();
+            command.add(listScript);
+            command.add(jsonFile.getPath());
+            command.add("--url");
+            command.add(url);
+            command.add("--id");
+            command.add(id);
+            command.add("--pwFile");
+            command.add(passwordFile.getPath());
+            command.add("--grp");
+            command.add("*");
+
+            Map<String, String> output = runProcess(command, 0);
+            Map<String, List<String>> expected = new HashMap<>();
+            expected.put(GROUP, Arrays.asList(LABEL));
+            expected.put(GROUP2, Arrays.asList(LABEL));
+            validateMigrationList(jsonFile, expected);
+        }
+
+        @Test
+        @Order(2)
+        void testWildcardSingleSegment() {
+            System.out.println("Running group wildcard single-segment tests.");
+            List<String> command = new ArrayList<>();
+            command.add(listScript);
+            command.add(jsonFile.getPath());
+            command.add("--url");
+            command.add(url);
+            command.add("--id");
+            command.add(id);
+            command.add("--pwFile");
+            command.add(passwordFile.getPath());
+            command.add("--grp");
+            command.add("Static*");
+
+            Map<String, String> output = runProcess(command, 0);
+            Map<String, List<String>> expected = new HashMap<>();
+            expected.put(GROUP, Arrays.asList(LABEL));
+            expected.put(GROUP2, Arrays.asList(LABEL));
+            validateMigrationList(jsonFile, expected);
+        }
+
+        @Test
+        @Order(5) // Execute last to prepare list for migration test
+        void testWildcardMultiSegment() {
+            System.out.println("Running group wildcard multi-segment tests.");
+            List<String> command = new ArrayList<>();
+            command.add(listScript);
+            command.add(jsonFile.getPath());
+            command.add("--url");
+            command.add(url);
+            command.add("--id");
+            command.add(id);
+            command.add("--pwFile");
+            command.add(passwordFile.getPath());
+            command.add("--grp");
+            command.add("*Static*Test");
+
+            Map<String, String> output = runProcess(command, 0);
+            Map<String, List<String>> expected = new HashMap<>();
+            expected.put(GROUP, Arrays.asList(LABEL));
+            validateMigrationList(jsonFile, expected);
+        }
+
+        @Test
+        @Order(3)
+        void testExactMatch() {
+            System.out.println("Running group exact match tests.");
+            List<String> command = new ArrayList<>();
+            command.add(listScript);
+            command.add(jsonFile.getPath());
+            command.add("--url");
+            command.add(url);
+            command.add("--id");
+            command.add(id);
+            command.add("--pwFile");
+            command.add(passwordFile.getPath());
+            command.add("--grp");
+            command.add("Static-Report-Migration-Test-2");
+
+            Map<String, String> output = runProcess(command, 0);
+            Map<String, List<String>> expected = new HashMap<>();
+            expected.put(GROUP2, Arrays.asList(LABEL));
+            validateMigrationList(jsonFile, expected);
+        }
+
+        @Test
+        @Order(4)
+        void testFileMatch() {
+            System.out.println("Running group from file tests.");
+            List<String> command = new ArrayList<>();
+            command.add(listScript);
+            command.add(jsonFile.getPath());
+            command.add("--url");
+            command.add(url);
+            command.add("--id");
+            command.add(id);
+            command.add("--pwFile");
+            command.add(passwordFile.getPath());
+            command.add("--grpf");
+            command.add(new File(testDir, "samples/groups.txt").getPath());
+
+            Map<String, String> output = runProcess(command, 0);
+            Map<String, List<String>> expected  = new HashMap<>();
+            expected.put(GROUP, Arrays.asList(LABEL));
+            expected.put(GROUP2, Arrays.asList(LABEL));
+            validateMigrationList(jsonFile, expected);
+        }
+    }
+
+    @Nested
+    @Order(2)
+    class MigrationsTests {
+        @Test
+        void migrationTest() {
+            System.out.println("Running migration test.");
+            // Assert json file has correct content from the previous test class
+            Map<String, List<String>> expected = new HashMap<>();
+            expected.put(GROUP, Arrays.asList(LABEL));
+            validateMigrationList(jsonFile, expected);
+
+            List<String> command = new ArrayList<>();
+            command.add(migrateScript);
+            command.add(jsonFile.getPath());
+            command.add("--url");
+            command.add(url);
+            command.add("--id");
+            command.add(id);
+            command.add("--pwFile");
+            command.add(passwordFile.getPath());
+            Map<String, String> output = runProcess(command, 0);
+            assertTrue(output.get("err").trim().isEmpty(), String.format("Error stream is not empty\nOUT:\n%s\n\nERR:\n%s", output.get("out"), output.get("err")));
+            validateResults();
+        }
+    }
+
+    private static void setupCollection() throws Exception {
+        System.out.println("Setting up collection.");
+        client.setErrorStatusCode(maxDeleteError);
+        client.deleteBuildResults(GROUP);
+        client.deleteCollection(GROUP);
+        client.deleteBuildResults(GROUP2);
+        client.deleteCollection(GROUP2);
+        client.setErrorStatusCode(maxNormError);
+        
+        client.createCollection(GROUP);
+        BuildResult newResult = client.createBuildResult(GROUP, LABEL);
+        newResult.setState(BuildResult.COMPLETE);
+
+        String samplesFolder = "samples/";
+        // Report data is labled with the version used to create it, in case of differences between versions
+        newResult.setBuildReportData(new FileInputStream(new File(testDir, samplesFolder + "result-data-1.1.3.json")));
+        newResult.setBuildReport(new FileInputStream(new File(testDir, samplesFolder + "report.html")));
+        newResult.save();
+
+        client.createCollection(GROUP2);
+        BuildResult newResult = client.createBuildResult(GROUP2, LABEL);
+        newResult.setState(BuildResult.COMPLETE);
+
+        // Report data is labled with the version used to create it, in case of differences between versions
+        newResult.setBuildReportData(new FileInputStream(new File(testDir, samplesFolder + "result-data-1.1.3.json")));
+        newResult.setBuildReport(new FileInputStream(new File(testDir, samplesFolder + "report.html")));
+        newResult.save();
+
+        // Assert file content
+        System.out.println("Asserting test file content.");
+        String metadataString = '{"date":"28-Feb-2022 17:26:26","build":"151","id":"DBB API Version","type":"VERSION","version":"1.1.3"}';
+        List<BuildResult> results = client.getAllBuildResults(Collections.singletonMap(RepositoryClient.GROUP, GROUP));
+        for (BuildResult result : results) {
+            assertEquals(GROUP, result.getGroup());
+            assertEquals(LABEL, result.getLabel());
+            assertTrue(Utils.readFromStream(result.fetchBuildReport(), "UTF-8").contains(metadataString));
+            assertTrue(Utils.readFromStream(result.fetchBuildReportData(), "UTF-8").contains(metadataString));
+        }
+        assertTrue(results.size() == 1);
+    }
+
+    private void validateMigrationList(File jsonFile, Map<String, List<String>> expected) {
+        JSONObject json;
+        try (BufferedReader reader = new BufferedReader(new FileReader(jsonFile))) {
+            json = JSONObject.parse(reader);
+        }
+
+        expected.forEach((key, value) -> {
+            assertTrue(json.containsKey(key));
+            assertIterableEquals(value, json.get(key));
+        });
     }
 
     private void validateResults() {
@@ -168,11 +346,7 @@ class StaticReportMigrationTests {
         }
     }
 
-    private Map<String, String> runMigrationScript(String command, int expectedRC) throws IOException, InterruptedException {
-        return runMigrationScript(Arrays.asList(command.split(" ")), expectedRC);
-    }
-
-    private Map<String, String> runMigrationScript(List<String> command, int expectedRC) throws IOException, InterruptedException {
+    private Map<String, String> runProcess(List<String> command, int expectedRC) throws IOException, InterruptedException {
         ProcessBuilder processBuilder = new ProcessBuilder(command);
         processBuilder.environment().put("DBB_HOME", EnvVars.getHome());
         
@@ -206,7 +380,16 @@ class StaticReportMigrationTests {
             error.append(line);
             error.append("\n");
         }
-        process.destroy();
+        if (process.isAlive()) {
+            int timeLeft = maxTime - (System.currentTimeMillis() - startTime);
+            if (timeLeft > 0) {
+                if (!process.waitFor(timeLeft, TimeUnit.MILLISECONDS)) {
+                    process.destroyForcibly();
+                }
+            } else {
+                process.destroyForcibly();
+            }
+        }
         
         int rc = process.exitValue();
         String errorString = error.toString();
