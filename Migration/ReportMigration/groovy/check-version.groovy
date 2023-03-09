16444b91d483603ee5d5939d21b3e27221b20411
Kai McGregor <kmcgreg@ibm.com>
2023-03-09 09:35:16 -0500

    Transferred changes from 2.x script
    Signed-off-by: Kai McGregor <kmcgreg@ibm.com>

--- Migration/ReportMigration/groovy/check-version.groovy
+++ Migration/ReportMigration/groovy/check-version.groovy
@@ -1,3 +1,12 @@
+import com.ibm.dbb.build.VersionInfo;
+
+/**
+ * Checks that the input version is equal to or greater than the input leastVersion.
+ * 
+ * @param version       The version number to check.
+ * @param leastVersion  The inclusive version number to check against.
+ * @return              True if the version is acceptable, false otherwise.
+ */
 boolean isVersionUnder(String version, String leastVersion) {
     int[] versionArr = Arrays.stream(version.split("\\.")).mapToInt(Integer::parseInt).toArray();
     int[] leastAcceptableVersion = Arrays.stream(leastVersion.split("\\.")).mapToInt(Integer::parseInt).toArray(); // Inclusive
@@ -25,6 +34,13 @@ boolean isVersionUnder(String version, String leastVersion) {
     return true;
 }
 
+/**
+ * Checks that the input version is less than the input mostVersion.
+ * 
+ * @param version       The version number to check.
+ * @param mostVersion  The exclusive version number to check against.
+ * @return             True if the version is acceptable, false otherwise.
+ */
 boolean isVersionOver(String version, String mostVersion) {
     int[] versionArr = Arrays.stream(version.split("\\.")).mapToInt(Integer::parseInt).toArray();
     int[] mostAcceptableVersion = Arrays.stream(mostVersion.split("\\.")).mapToInt(Integer::parseInt).toArray(); // Non-inclusive
@@ -48,4 +64,31 @@ boolean isVersionOver(String version, String mostVersion) {
         if (versionArr[1] == mostAcceptableVersion[1] && versionArr[2] >= mostAcceptableVersion[2]) return false;
     }
     return true;
+}
+
+/**
+ * Retrieves the version from the DBB API and checks it against the input version strings.
+ * 
+ * @param leastAcceptableVersion    The least acceptable version (inclusive).
+ * @param mostAcceptableVersion     The most acceptable version (exclusive).
+ * @return                          A filled error message if a check failed, an empty string otherwise.
+ */
+String checkVersion(String leastAcceptableVersion, String mostAcceptableVersion) {
+    String version = getVersion();
+    String errorMessage;
+    if (!isVersionUnder(version, leastAcceptableVersion)) {
+        errorMessage = String.format("DBB Version %s is not compatable with this tool, please upgrade to version >= %s", version, leastAcceptableVersion);
+    } else if (!isVersionOver(version, mostAcceptableVersion)) {
+        errorMessage = String.format("DBB Version %s is not compatable with this tool, please use the version of this tool compatable with DBB %s", version, leastAcceptableVersion);
+    }
+    return errorMessage;
+}
+
+/**
+ * Returns the DBB version from its API.
+ * 
+ * @return  the current version of DBB.
+ */
+String getVersion() {
+    return VersionInfo.getInstance().getVersion();
 }
