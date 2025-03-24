c80fb56a1bcc8b3716e8d27efed9ae7dea93b56c
Monvorath Phongpaibul <monvorath@Monvoraths-MacBook-Pro.local>
2025-03-24 15:44:48 +0700

    test computeReleaseVersion.sh

--- Templates/Common-Backend-Scripts/computeReleaseVersion.sh
+++ Templates/Common-Backend-Scripts/computeReleaseVersion.sh
@@ -71,7 +71,7 @@ if [ $rc -eq 0 ]; then
 
 # Get Options
     if [ $rc -eq 0 ]; then
-        while getopts "h:w:a:b:p:r" opt; do
+        while getopts "h:w:a:b:p:t" opt; do
             case $opt in
             h)
                 Help
@@ -120,13 +120,13 @@ if [ $rc -eq 0 ]; then
                 fi
                 PipelineType="$argument"
                 ;;
-            r) 
+            t) 
                 # release type
                 argument="$OPTARG"
                 nextchar="$(expr substr $argument 1 1)"
                 if [ -z "$argument" ] || [ "$nextchar" = "-" ]; then
                   rc=4
-                  INFO=$PGM": [WARNING] Release type is required (-q). rc="$rc
+                  INFO=$PGM": [WARNING] Release type is required (-t). rc="$rc
                   echo $INFO
                   break
                 fi
