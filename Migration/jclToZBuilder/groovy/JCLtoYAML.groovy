9d03d26a82ac6f341e6724dcd280df666c93da38
Kai McGregor <kmcgreg@ibm.com>
2025-05-06 10:03:36 -0400

    Removed unused CLI option
    Signed-off-by: Kai McGregor <kmcgreg@ibm.com>

--- Migration/jclToZBuilder/groovy/JCLtoYAML.groovy
+++ Migration/jclToZBuilder/groovy/JCLtoYAML.groovy
@@ -945,7 +945,7 @@ def parseArgs(String[] args) {
 	def cli = new CliBuilder(usage:usage)
 		  cli.c(longOpt: 'configFolder',    args:1, argName: 'configFolder',                optionalArg:false,  'Path to the config folder containing the JCL migration configuration file and the dataset mappings configuration file.  If specified, path is considered absolute if it begins with a slash else it is relative path from the migration tool bin directory.  Default is ../conf/.')
 		  cli.d(longOpt: 'dataset',       args:1, argName: 'MVS dataset',                   optionalArg:false, 'Dataset containing JCL to be migrated (Required)')
-		  cli.g(longOpt: 'genExecVars',   args:1, argName: 'Generate executable variables', optionalArg:true,  'Specify true to generate executable variables')
+		  //cli.g(longOpt: 'genExecVars',   args:1, argName: 'Generate executable variables', optionalArg:true,  'Specify true to generate executable variables')
 		  cli.h(longOpt: 'help',                                                                               'Show usage information')
 		  cli.m(longOpt: 'member',        args:1, argName: 'JCL member',                    optionalArg:false, 'JCL member being migrated (Required)')
 		  cli.o(longOpt: 'outputDir',     args:1, argName: 'output directory',              optionalArg:true,  'Directory in the HFS where all files will be written. If specified, path is considered absolute if it begins with a slash else it is relative path from the users home directory.  Default is jclMigration.')
