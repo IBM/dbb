ca4ac49ccb93ad75cf2aea406352be37b369d5b9
Luke Burgess <luke.burgess@ibm.com>
2025-10-14 12:08:06 -0400

    use library mappings configuration variable
    Signed-off-by: Luke Burgess <luke.burgess@ibm.com>

--- zBuilder/ProcessDeletedFiles/groovy/ProcessDeletedFiles.groovy
+++ zBuilder/ProcessDeletedFiles/groovy/ProcessDeletedFiles.groovy
@@ -9,15 +9,22 @@
 * build output datasets.
 */
 
+// check there are deleted files to process
 Set<String> deletedFiles = context.getSetStringVariable(TaskConstants.DELETED_FILES);
 if (deletedFiles == null || deletedFiles.isEmpty()) {
-   println "> No deleted files to process."
+   println ">> No deleted files to process."
    return 0
 }
 
-deletedFiles.each { String deletedFile ->
-    //println "Creating deletion record for file: ${deletedFile}"
+// parse output library mappings from config
+Map<PathMatcher, List<String>> libMappings = parseOutputLibraryMappings(config.getListVariable("outputLibMappings"))
+if (outputLibMappings == null || outputLibMappings.isEmpty()) {
+        println ">> ERROR: 'outputLibMappings' not defined in task configuration."
+        return -1
+}
 
+// process each deleted file
+deletedFiles.each { String deletedFile ->
     // Create a new AnyTypeRecord for the deleted file
     AnyTypeRecord deleteRecord = new AnyTypeRecord("DELETE_RECORD")
     deleteRecord.setAttribute("file", deletedFile)
@@ -25,22 +32,27 @@ deletedFiles.each { String deletedFile ->
     List<String> deletedOutputsList = new ArrayList<String>() 
 
     String member = CopyToPDS.createMemberName(deletedFile)
-    List<String> outputLibs = config.getListStringFileVariable("outputLibs", deletedFile)
-    if (outputLibs != null && !outputLibs.isEmpty()) {
-        outputLibs.each { String outputLib ->
-            // add fully qualified dsn to deleted outputs list
-            String pdsMember = "${outputLib}(${member})"
-            deletedOutputsList.add(pdsMember)
-            println "> Document deletion ${pdsMember} for file ${deletedFile}"
 
-            // delete member from build datasets
-            if (ZFile.dsExists("//'$pdsMember'")) {
-                if (props.verbose) println ">> Deleting ${pdsMember}"
-                ZFile.remove("//'$pdsMember'")
+    libMappings.keySet().each { PathMatcher matcher ->
+        if (matcher.matches(Paths.get(deletedFile))) {
+            List<String> outputLibs = libMappings.get(matcher)
+            if (outputLibs != null && !outputLibs.isEmpty()) {
+                outputLibs.each { String outputLib ->
+                    // add fully qualified dsn to deleted outputs list
+                    String pdsMember = "${outputLib}(${member})"
+                    deletedOutputsList.add(pdsMember)
+                    println "> Document deletion ${pdsMember} for file ${deletedFile}"
+                    
+                    // delete member from build datasets
+                    if (ZFile.dsExists("//'$pdsMember'")) {
+                        if (props.verbose) println ">> Deleting ${pdsMember}"
+                        ZFile.remove("//'$pdsMember'")
+                    }
+                }
+            } else {
+                println "> No outputLibs configured for file ${deletedFile}. No record created."
             }
         }
-    } else {
-        println "> No outputLibs configured for file ${deletedFile}. No deletion records created."
     }
 
     
@@ -50,3 +62,38 @@ deletedFiles.each { String deletedFile ->
     }
 }
 
+
+
+// helper function to parse output library mappings from config
+Map<PathMatcher, List<String>> parseOutputLibraryMappings(List<Object> rawMaps) {
+    // get outputLibMappings from config
+    if (rawMaps == null || rawMaps.isEmpty()) {
+        return null
+    }
+    // parse into map of source to list of outputLibs
+    Map<PathMatcher, List<String>> libMappings = new LinkedHashMap<>()
+    try {
+        rawMaps.each { Object rawMap ->
+            Map map = (Map) rawMap
+            String source = map.get("source")
+            List<String> outputLibs = map.get("outputLibs")
+            if (source != null && outputLibs != null && !outputLibs.isEmpty()) {
+                PathMatcher pathMatcher = getPathMatcher(source)
+                libMappings.put(pathMatcher, outputLibs)
+            } else {
+                println ">> WARNING: Invalid library mapping entry. 'source' or 'outputLibs' missing or empty."
+            }
+        }
+    } catch (Exception e) {
+        println ">> ERROR: Library mappings format invalid. Parsing error: ${e.getMessage()}"
+        return null
+    }
+    return libMappings
+}
+
+// helper function to create PathMatcher from glob or regex pattern
+PathMatcher getPathMatcher(String pattern) {
+    if (!pattern.startsWith("glob:") && !pattern.startsWith("regex:"))
+        pattern = "glob:" + pattern;
+    return FileSystems.getDefault().getPathMatcher(pattern);
+}
