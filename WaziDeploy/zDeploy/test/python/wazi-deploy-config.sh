b9df5638f96a1346b42c1d0eb4d184c21b8e374c
Dennis Behm <dennis.behm@de.ibm.com>
2026-03-06 09:53:36 +0100

    Update Test
    Signed-off-by: Dennis Behm <dennis.behm@de.ibm.com>

--- WaziDeploy/zDeploy/test/python/wazi-deploy-config.sh
+++ WaziDeploy/zDeploy/test/python/wazi-deploy-config.sh
@@ -11,7 +11,7 @@ export TMPHLQ="DBEHM"
 # Python Virtual Environment Setup
 echo "[INFO] - Setup Python virtual environment."
 unset PYTHONPATH
-. /var/python-envs/wazi-deploy-v3.0.7_preview/bin/activate
+. /var/python-envs/wazi-deploy-v3.0.7.1/bin/activate
 
 # ZOAU (Z Open Automation Utilities) Configuration
 export ZOAU_HOME=/var/zoau-1.3.6.0
@@ -29,9 +29,11 @@ export APPLICATION="base"
 
 # Package URL
 export PACKAGE_URL="http://10.3.20.231:8081/artifactory/base-dbehm-repo-local/release/rel-1.0.0/base-rel-1.0.0-2025-06-27_08-52-10.tar"
+export PACKAGE_URL="http://10.3.20.231:8081/artifactory/cbsa-zos-native-gitlabr-repo-local/release/rel-3.7.0/cbsa-zos-native-rel-3.7.0-13008.tar"
 
 echo "[INFO] - Wazi Deploy environment configured:"
-echo "  - Virtual Environment: wazi-deploy-v3.0.7_preview"
+echo "  - Virtual Environment: "
+wazideploy-deploy -v
 echo "  - ZOAU Home: $ZOAU_HOME"
 echo "  - User HLQ: $TMPHLQ"
 echo "  - Target HLQ: $TARGET_HLQ"
